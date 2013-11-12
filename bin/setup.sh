857fb15b7329076fc18963e3869a732758df1883
David Workman <workmad3@gmail.com>
2013-11-12 11:40:39 +0000

    Adding database.yml example that's suitable for a standard postgres setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 bundle
 createuser kimcheesey -s
+if [[ ! -e config/database.yml ]]; then
+  cp config/database.example.yml config/database.yml
+fi
 if [[ ! -e config/application.yml ]]; then
   cp config/application.example.yml config/application.yml
 fi
