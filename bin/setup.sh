985e5a6bba392774e6702852aa3680227c171e4c
David Workman <workmad3@gmail.com>
2013-11-12 11:37:32 +0000

    Adding a little bit extra to the setup.sh script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 bundle
 createuser kimcheesey -s
+if [[ ! -e config/application.yml ]]; then
+  cp config/application.example.yml config/application.yml
+fi
 rake db:create:all db:migrate db:test:prepare db:seed
